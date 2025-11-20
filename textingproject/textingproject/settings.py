e90c9ff19578064f799edb855e45fcffba41cf4a
Brysonflowers <101665972+Brysonflowers@users.noreply.github.com>
2025-11-20 11:16:53 -0600

    testing if it pushes

--- textingproject/textingproject/settings.py
+++ textingproject/textingproject/settings.py
@@ -11,6 +11,20 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',  
+    messages.INFO: 'alert-info', 
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger', 
+}
+
+LOGIN_REDIRECT_URL = '/'
+LOGIN = 'login'
+LOGOUT = 'logout'
+LOGIN_URL = 'login'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
