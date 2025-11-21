313952a31f7cb2d49a08f9f8a061f19884267939
spadilla11 <dpadilla26@basecampcodingacademy.org>
2025-11-21 15:37:43 -0600

    moved the navbar and made it as a manu

--- textingproject/echochatapp/urls.py
+++ textingproject/echochatapp/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
-from .views import *
+from django.contrib import admin
+from django.urls import path, include
+from echochatapp.views import *
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('', index, name = 'home'),
     path("sign_up/", SignUpView.as_view(), name="signup"),
     path("create_group/", create_group, name="create_group"),
+    path('chat',thecurrentchatviewer,name='chat'),
+    path('private chats', private_chats_view, name = 'private_chats'),
+    path('chatting page', chatting_page_view, name='chatting_page'),
+    path("accounts/", include('django.contrib.auth.urls')),
 ]
