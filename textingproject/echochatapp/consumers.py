68feddb69bedda63c05bf1f7d9554ce76337dd77
Brysonflowers <101665972+Brysonflowers@users.noreply.github.com>
2025-11-29 17:09:21 -0600

    one import is wrong I fixed it

--- textingproject/echochatapp/consumers.py
+++ textingproject/echochatapp/consumers.py
@@ -1,6 +1,7 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import sync_to_async
+from asgiref.sync import sync_to_async
+# from channels.db import sync_to_async
 from .models import ChatMessage
 from django.contrib.auth.models import User
 
