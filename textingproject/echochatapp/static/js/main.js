57062a3d21874fd20158a4bd2c2157cd0b54035c
spadilla11 <dpadilla26@basecampcodingacademy.org>
2025-11-29 20:00:56 -0600

    added javascript to the side bar

--- textingproject/echochatapp/static/js/main.js
+++ textingproject/echochatapp/static/js/main.js
@@ -1,37 +1,11 @@
-const menuToggleButton = document.getElementById('menu-toggle-btn');
-const navbar = document.getElementById('nav-bar');
+const menuButton = document.getElementById('menu-toggle-btn');
+const slideMenu = document.getElementById('nav-bar');
 
-document.addEventListener('DOMContentLoaded', () => {
-    const toggle = document.getElementById("theme-toggle");
-
-    // Load saved mode
-    if (localStorage.getItem("dark") === "true") {
-        document.body.classList.add("dark-mode");
-        toggle.textContent = "Light Mode";
-    }
-
-    else if (localStorage.getItem("dark") === "false") {
-        toggle.textContent = "Dark Mode";
-    }
-
-    toggle.addEventListener("click", () => {
-        document.body.classList.toggle("dark-mode");
-
-        const dark = document.body.classList.contains("dark-mode");
-        localStorage.setItem("dark", dark);
-
-        toggle.textContent = dark ? "Light Mode" : "Dark Mode";
-    });
-})
-
-function home_page() {
-    document.location.href = "http://127.0.0.1:8000"
-}
-
-menuToggleButton.addEventListener('click', () => {
-  // Toggle the 'hidden' class to show/hide the navbar
-  navbar.classList.toggle('visisble');
-
-  // Alternative using 'visible' class:
-  // navbar.classList.toggle('visible');
+menuButton.addEventListener('click', () => {
+    slideMenu.classList.toggle('open');
 });
+
+
+
+
+
