9d2059ddffb86ccf412c18ad14c3819f8e14074e
Brysonflowers <101665972+Brysonflowers@users.noreply.github.com>
2025-11-30 14:27:16 -0600

    I did the last touches

--- textingproject/echochatapp/static/js/main.js
+++ textingproject/echochatapp/static/js/main.js
@@ -14,3 +14,49 @@ if (menuToggleButton) {
       // navbar.classList.toggle('visible');
     });
 }
+
+const roomNameElement = document.getElementById('room-name');
+if (roomNameElement) {
+    const roomName = JSON.parse(roomNameElement.textContent);
+    const chatLog = document.querySelector('#chat-log');
+    const messageInput = document.querySelector('#chat-message-input');
+    const messageSubmit = document.querySelector('#chat-message-submit');
+
+    const chatSocket = new WebSocket(
+        'ws://'
+        + window.location.host
+        + '/ws/chat/'
+        + roomName
+        + '/'
+    );
+
+    chatSocket.onmessage = function(e) {
+        const data = JSON.parse(e.data);
+        const messageElement = document.createElement('div');
+        messageElement.classList.add('message');
+        messageElement.innerHTML = `<span class="username">${data.username}:</span> <span class="text">${data.message}</span>`;
+        chatLog.appendChild(messageElement);
+        chatLog.scrollTop = chatLog.scrollHeight; // Scroll to the bottom
+    };
+
+    chatSocket.onclose = function(e) {
+        console.error('Chat socket closed unexpectedly');
+    };
+
+    messageInput.focus();
+    messageInput.onkeyup = function(e) {
+        if (e.keyCode === 13) {  // enter, return
+            messageSubmit.click();
+        }
+    };
+
+    messageSubmit.onclick = function(e) {
+        const message = messageInput.value;
+        if (message.trim() !== '') {
+            chatSocket.send(JSON.stringify({
+                'message': message
+            }));
+            messageInput.value = '';
+        }
+    };
+}
